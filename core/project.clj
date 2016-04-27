339cd26b257e0cfb630a12986d5c276ab3d383e3
jhony-chikens <evts85@gmail.com>
2016-04-27 13:23:09 +0200

    Fixes #2 add docker image build functionality

--- core/project.clj
+++ core/project.clj
@@ -4,9 +4,11 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [
-    [org.clojure/clojure "1.8.0"]
-    [clj-http "2.1.0"]
-    [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/clojure "1.8.0"]
+                 [clj-http "2.1.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [com.github.docker-java/docker-java "2.1.2"]
+                 [org.apache.commons/commons-compress "1.8"]]
   :profiles {:dev {:dependencies [
                                   [midje "1.6.3"]
                                   [clj-http-fake "1.0.2"]]}})
