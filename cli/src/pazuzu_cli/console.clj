275fd49821a9290cbf4194187ca76891ed1f590b
Nikhil <nikhil.anurag@zalando.de>
2016-05-18 19:01:02 +0200

    #13 Tests for saving Dockerfile can be written now. All the setup & cleanup code is ready.

--- cli/src/pazuzu_cli/console.clj
+++ cli/src/pazuzu_cli/console.clj
@@ -1,8 +1,8 @@
 (ns pazuzu-cli.console
   (:require [pazuzu-core.docker :as docker]
-                                [pazuzu-core.registry-client :as registry]
-                                [midje.sweet :refer :all]
-                                [clojure.data.json :as json]))
+            [pazuzu-core.registry-client :as registry]
+            [midje.sweet :refer :all]
+            [clojure.data.json :as json]))
 
 (defn fetch-and-compile-features
   "CLI should be able to fetch the features & compile it into a single Dockerfile.
@@ -13,9 +13,8 @@
               any-url)]
     ((comp docker/generate-dockerfile registry/get-sorted-features) url features)))
 
-;TODO: Figure out how to temporarily save and check for saved docker file.
 (defn save-docker-file
-  [docker-file-data  & path]
+  [docker-file-data  [& path]]
   (let [save-path (case path
                     nil (str "Dockerfile-" (System/currentTimeMillis))
                     path)]
--- cli/test/pazuzu_cli/console_test.clj
+++ cli/test/pazuzu_cli/console_test.clj
@@ -2,7 +2,12 @@
   (:require [midje.sweet :refer :all]
             [pazuzu-cli.console :refer :all]
             [clj-http.client :as client]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helper fcns for testing console/fetch-and-compile-features
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def test-url "http://localhost:8080/api/features")
 
@@ -15,7 +20,7 @@
   {:py-scala [{:name "java" :docker_data "Java-Data" :test_instruction "jvm-test"}
               {:name "scala" :docker_data "scala" :test_instruction "scala-data"}
               {:name "Python" :docker_data "Python-Data" :test_instruction "py-test"}]
-  :empty    ""})
+   :empty    ""})
 
 ; Generate the response for given key
 (defn http-response
@@ -24,20 +29,42 @@
    :headers {}
    :body (json/write-str (query-key return-response))})
 
-; Make this work! Macros?
-;(defn test-get [key]
-;  #(client/get test-url {:query-params (key params-map)}) => (http-response key))
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Tests for console/fetch-and-compile-features
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (fact "we have a proper docker file"
       (fetch-and-compile-features ["Python", "scala"]) => (str "# Auto-generated DockerFile by Pazuzu2\n\n"
                                                                "FROM ubuntu:latest\n\n# java\nJava-Data\n\n"
                                                                "# scala\nscala\n\n# Python\nPython-Data\n\nCMD /bin/bash")
       ;(fetch-and-compile-features []) => ""
       (provided
-        (client/get test-url {:query-params (:py-scala params-map)}) => (http-response :py-scala)))
-      ;(client/get test-url {:query-params (:empty params-map)}) => (http-response :empty)
+        (client/get test-url {:query-params (:py-scala params-map )}) => (http-response :py-scala)))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helper fcns for testing console/save-docker-file.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn get-test-docker-files
+  [dir-path]
+  (filter #(re-find #"Dockerfile-" %)
+          (map #(str %) (file-seq (io/file dir-path)))))
 
+(defn clean-up-docker-files
+  [dir-path]
+  (let
+     [test-docker-files (get-test-docker-files dir-path)]
+    (println test-docker-files)
+    (map #(io/delete-file %) test-docker-files)))
+
+(def docker-data "Hello This is docker Data")
+
+(defn save-and-check-for-dockerfile
+  [& path]
+  (let [saved-file-name (save-docker-file docker-data path)]
+    (.exists (io/as-file saved-file-name))))
+
+(with-state-changes [(after :facts (println (clean-up-docker-files ".")))]
+                    (fact "Dockerfile is generated for given docker-data"
+                                                                       (save-and-check-for-dockerfile) => true))
 
 
