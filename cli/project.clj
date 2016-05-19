98792ed756ed2d09668e837621e44f9749606aed
Ent <last_ent@outlook.com>
2016-05-19 14:21:52 +0200

    #13 Removing licenses at lein project levels (core & cli).

--- cli/project.clj
+++ cli/project.clj
@@ -1,8 +1,8 @@
 (defproject pazuzu-cli "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [pazuzu-core "0.1.0-SNAPSHOT"]
                  [org.clojure/data.json "0.2.6"]]
--- core/project.clj
+++ core/project.clj
@@ -4,8 +4,9 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [
-    [org.clojure/clojure "1.8.0"]
-    [clj-http "2.1.0"]
-    [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/clojure "1.8.0"]
+                 [clj-http "2.1.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :profiles {:dev {:plugins      [[lein-midje "3.2"]]
                    :dependencies [[midje "1.8.3"]]}})
+
