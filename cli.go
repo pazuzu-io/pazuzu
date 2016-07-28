b041a5c6affe1544392c89f7b903437aa292bd77
Sasidharan <sasidharan.sambasivam@zalando.de>
2016-07-28 11:41:58 +0200

    Revert "Changes to include base image option in cli"
    This reverts commit fc52a610b05871412a450ceadab211c0ef8bf30b.

--- cli.go
+++ cli.go
@@ -21,11 +21,6 @@ var buildCmd = cli.Command{
 			Value: "pazuzu-img",
 			Usage: "Set docker image name",
 		},
-                cli.StringFlag{
-                        Name:  "base-image, b",
-                        Value: "ubuntu",
-                        Usage: "Set the docker base image",
-                },
 		cli.StringFlag{
 			Name:  "test-spec, t",
 			Value: "test_spec.json",
@@ -58,7 +53,7 @@ func buildFeatures(c *cli.Context) error {
 		return fmt.Errorf("no features specified")
 	}
 
-	err := pazuzu.Generate(c.String("base-image"),c.Args())
+	err := pazuzu.Generate(c.Args())
 	if err != nil {
 		return err
 	}
