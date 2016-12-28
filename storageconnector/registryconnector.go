3479c70447c540afbd8206c297737478037b9295
Olivier Boure <olivier.boure@gmail.com>
2016-12-28 16:23:48 +0100

    Pazuzu-138 updated the registry connector with the new API

--- storageconnector/registryconnector.go
+++ storageconnector/registryconnector.go
@@ -1,15 +1,18 @@
 package storageconnector
 
 import (
-	"github.com/zalando-incubator/pazuzu/shared"
 	"regexp"
 	"strconv"
 
 	"swaggen/client/features"
+	"swaggen/client/feature_metas"
+	"github.com/zalando-incubator/pazuzu/shared"
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/strfmt"
 
 	httptransport "github.com/go-openapi/runtime/client"
+	"swaggen/models"
+	"context"
 )
 
 type registryStorage struct {
@@ -18,6 +21,7 @@ type registryStorage struct {
 	Token string     // OAUTH2 Token
 
 	Features 	*features.Client
+	Metas		*feature_metas.Client
 	Transport	runtime.ClientTransport
 }
 
@@ -33,6 +37,7 @@ func (store *registryStorage) init(hostname string, port int, formats strfmt.Reg
 
 	store.Transport = transport
 	store.Features = features.New(transport, formats)
+	store.Metas = feature_metas.New(transport, formats)
 }
 
 func NewRegistryStorage(hostname string, port int, formats strfmt.Registry) (*registryStorage, error) {
@@ -48,6 +53,7 @@ func NewRegistryStorage(hostname string, port int, formats strfmt.Registry) (*re
 // Return a full feature data from the storage.
 // For the registry, the filtering is done server-side to reduce result size.
 // name:	a value, that must present in feature name (from API doc)
+// TODO untested function !
 func (store *registryStorage) GetFeature(name string) (shared.Feature, error) {
 
 	// let's get features containing name on the registry
@@ -70,8 +76,9 @@ func (store *registryStorage) GetFeature(name string) (shared.Feature, error) {
 
 // Use the given regex to return a list of FeatureMeta.
 // name		a regex used to filter out FeatureMeta
-// TODO : issue #137 -> implement Features Meta on the registry for speed gain
-// TODO : issue registry-#111 -> investigate possibility of regex support server-side to optimize
+// TODO untested function !
+// TODO issue #138 -> update method with the new API
+// TODO issue registry-#111 -> investigate possibility of regex support server-side to optimize
 func (store *registryStorage) SearchMeta(name *regexp.Regexp) ([]shared.FeatureMeta, error) {
 
 	result := []shared.FeatureMeta{}
@@ -93,49 +100,71 @@ func (store *registryStorage) SearchMeta(name *regexp.Regexp) ([]shared.FeatureM
 
 // Return a feature metadata from the storage.
 // name:	a value, that must present in feature name
-// TODO : issue #137 -> implement Features Meta on the registry for speed gain
+// TODO untested function !
 func (store *registryStorage) GetMeta(name string) (shared.FeatureMeta, error) {
-	ft, err := store.GetFeature(name)
-	return ft.Meta, err
+
+	params := feature_metas.NewGetAPIFeatureMetasNameParams()
+	params.Name = name
+
+	meta, err := store.Metas.GetAPIFeatureMetasName(params)
+
+	if err != nil {
+		return shared.FeatureMeta{}, err
+	}
+	return shared.NewMeta(meta.Payload), nil
 }
 
 // Resolve a list of features and their dependencies from the storage. Return non-nil err if at least one feature not found.
 // names:	an array of feature names
+// TODO untested function !
 func (store *registryStorage) Resolve(names ...string) ([]string, map[string]shared.Feature, error) {
+
+	params := features.NewGetAPIResolvedFeaturesParams()
+	params.Name = names
+
+	features, err := store.Features.GetAPIResolvedFeatures(params)
+	if err != nil {
+		return []string{}, map[string]shared.Feature{},err
+	}
+
 	var slice []string
 	result := map[string]shared.Feature{}
 
-	for _,name := range names {
-		err := store.resolve(name, &slice, result)
-		if err != nil {
-			return []string{}, map[string]shared.Feature{}, err
-		}
+	for _,feature := range features.Payload {
+
+		feature2 := shared.NewFeature(feature)
+		name := feature2.Meta.Name
+		result[name] = feature2
+		slice = append(slice, name)
 	}
 	return slice, result, nil
 }
 
-// Resolve a single feature and its dependencies from the storage. Return non-nil err if feature not found.
-// name:	a value that must present in feature name
-// list:	a list of previously visited feature ids
-// result:	a result map containing feature ids and data
-func (store *registryStorage) resolve(name string, list *[]string, result map[string]shared.Feature) error {
-	// if name already in result
-	if _, ok := result[name]; ok {
-		return nil
+func (store *registryStorage) addFeature(feature shared.Feature) error {
+
+
+	meta := models.FeatureMeta{
+		Name: feature.Meta.Name,
+		Description: feature.Meta.Description,
+		Author: feature.Meta.Author,
+		Dependencies: feature.Meta.Dependencies,
+		UpdatedAt: feature.Meta.UpdatedAt.UTC().Format("2006-01-02T15:04:05-0700"),
 	}
 
-	feature, err := store.GetFeature(name)
+	newfeature := models.Feature{
+		Meta: &meta,
+		Snippet: feature.Snippet,
+		TestSnippet: feature.TestSnippet,
+	}
+
+	params := features.PostAPIFeaturesParams{
+		Feature: &newfeature,
+		Context: context.Background(),
+	}
+
+	_, err := store.Features.PostAPIFeatures(&params)
 	if err != nil {
 		return err
 	}
-	for _, depName := range feature.Meta.Dependencies {
-		err := store.resolve(depName, list, result)
-		if err != nil {
-			return err
-		}
-	}
-
-	result[name] = feature
-	*list = append(*list, name)
 	return nil
 }
