00b6193900969e1b6e4f1027c47cbd434041b6a8
Rafael Santos Bezerra (@rafaelsbz) <rafaelsbz@gmail.com>
2016-12-29 14:16:06 +0100

    Reverts tests back to normal

--- api_test.go
+++ api_test.go
@@ -42,8 +42,6 @@ func TestGetFeatures(t *testing.T) {
 	if len(fs) != 1 {
 		t.Errorf("expected 1 feature, got %d", 1, len(fs))
 	}
-
-	t.Error("Forced test failure.", nil)
 }
 
 // Test getting error response from API.
