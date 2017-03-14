d1befd61cc255415637490686f26cf13772256a5
Dmytro Melnychenko <dmytro.i.am@gmail.com>
2017-03-14 13:52:20 +0100

    Add clean command

---
+++ cli/pazuzu/command/clean/clean.go
@@ -0,0 +1,32 @@
+package clean
+
+import (
+  "fmt"
+  "os"
+  "github.com/urfave/cli"
+  "github.com/zalando-incubator/pazuzu"
+  "github.com/zalando-incubator/pazuzu/shared"
+)
+
+var Command = cli.Command{
+  Name: "clean",
+  Aliases: []string{"cl"},
+  Usage: "remove Pazuzufile and Dockerfile",
+  Action: cleanAction,
+}
+
+func cleanAction(c *cli.Context) error {
+  err := os.Remove(pazuzu.PazuzufileName)
+  if err != nil {
+    fmt.Println(err)
+  }
+  err = os.Remove(pazuzu.DockerfileName)
+  if err != nil {
+    fmt.Println(err)
+  }
+  err = os.Remove(shared.TestSpecFilename)
+  if err != nil {
+    fmt.Println(err)
+  }
+  return nil
+}
